d7ebfdb92abc1ee1998a9a5e19fd5d93acd33410
Trevor Hullinger <hakoniw@gmail.com>
2015-02-03 14:50:46 -0700

    Added Skeleton; moved away from a tutorial application to my own.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
 // Tutorial application for now.
-// Most of the code used was found here:
-// http://code.tutsplus.com/tutorials/introduction-to-html5-desktop-apps-with-node-webkit--net-36296
 
 var fs = require('fs'),
     file = require('./js/file.js'),
@@ -16,44 +14,49 @@ var d = React.DOM;
 
 var data = immstruct({ numbers: {} });
 
-var Bucket = component('Bucket', function (props, statics) {
-  var numbers = props.number.toArray();
-  return d.li({},
-              d.b({}, 'Bucket #', statics.label, ' '),
-              d.span({}, '(', numbers.reduce(function (acc, n) {
-                return acc + 1;
-              }, 0), ')'),
-              ': ',
-              numbers.map(function (number, key) {
-                return number;
+var test_data = immstruct({
+  posts: [
+  { title: "HELLO",
+    content: "Super cool microblog post." },
+  { title: "Hello....",
+    content: "Another super cool microblog post."}
+  ]
+});
+
+var Post = component('Post', function (props) {
+  var post = props.cursor.toJS();
+  return d.div({className: 'container'},
+           d.div({className: 'row'},
+             d.div({className: 'one-half column'},
+               d.h2({}, post.title),
+               d.p({}, post.content))
+         ));
+});
+
+
+var Posts = component('Posts', function (props) {
+  var posts = props.cursor.toArray();
+  return d.ul({},
+              posts.map(function (post) {
+                return Post(post);
               }));
 });
 
-var Buckets = component('Buckets', function (props) {
-  var labels = Object.keys(props.cursor.toJS());
-  return d.ul({},
-              props.cursor.toArray().map(function (number, i) {
-                return Bucket('bucket-' + i, { number: number, statics: { label: labels[i] } });
-              }));
-});
 
 function render () {
   React.render(
-    Buckets(data.cursor('numbers')),
+    Posts(test_data.cursor('posts')),
+    // Buckets(data.cursor('numbers')),
     document.getElementById('app'));
 }
 
 render();
-data.on('swap', render);
+test_data.on('swap', render);
 
 setInterval(function () {
-  var bucket = parseInt(Math.random() * 30);
-  var number = parseInt(Math.random() * 10);
-
-  data.cursor(['numbers', bucket]).update(function (state) {
-    if (!state) {
-      return Immutable.List.of(number);
-    }
-    return state.unshift(number);
+  var new_title = "This is another.";
+  var new_content = "Please go away JS.";
+  test_data.cursor('posts').update(function (post_list) {
+    return post_list.push(Immutable.Map({ title: new_title, content: new_content }));
   });
-}, 30);
+}, 1000);
