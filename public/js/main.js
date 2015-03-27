fce1405c33186d54b503b08e5ed62efa7a3c8128
Trevor Hullinger <hakoniw@gmail.com>
2015-03-27 02:35:58 -0600

    Reformat React DOM elements because web-beautify doesn't work well with them

--- public/js/main.js
+++ public/js/main.js
@@ -49,28 +49,16 @@ var MainApp = component('MainApp', function (props) {
       return !flag;
     });
   }
-  return d.div({
-      id: 'main-app',
-      className: (data.showSidebar) ? 'show-nav' : ''
-    }, d.div({
-      id: 'canvas'
-    }, d.div({
-        id: 'sidebar-menu'
-      }, d.h2({}, 'Subscriptions'),
-
-      Sidebar({
-        subscriptions: data.subscriptions
-      })), 
-      d.a({
-        href: '#',
-        className: 'toggle-nav',
-        onClick: toggleSidebar
-      }, d.i({
-	id: 'toggle-icon',
-        className: 'fa fa-bars fa-lg'
-      })), Feed(data.stream))
-
-  );
+  return d.div({id: 'main-app', className: (data.showSidebar) ? 'show-nav' : ''},
+	       d.div({id: 'canvas'},
+		     d.div({id: 'sidebar-menu'},
+			   d.h2({}, 'Subscriptions'),
+			   Sidebar({subscriptions: data.subscriptions})),
+		     d.a({href: '#',
+			  className: 'toggle-nav',
+			  onClick: toggleSidebar},
+			 d.i({id: 'toggle-icon', className: 'fa fa-bars fa-lg'})),
+		     Feed(data.stream)));
 });
 
 function render() {
