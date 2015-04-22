fd38ea936777ec3f1b2242ba91b78f4a08681166
Trevor Hullinger <hakoniw@gmail.com>
2015-04-22 07:21:53 -0600

    Work on gallery view

--- src/jsx/gallery.jsx
+++ src/jsx/gallery.jsx
@@ -1,26 +1,30 @@
 var React = require('react'),
-    query = require('./domquery');
+    query = require('./domquery'),
+    ImagesLoaded = require('imagesloaded');
 
 module.exports = React.createClass({
 
   componentDidMount: function () {
       var container = document.querySelector('#gallery');
       var msnry = new Masonry( container, {
-          columnWidth: 100,
+          columnWidth: 10,
           itemSelector: '.image-post'
+      });
+      ImagesLoaded(container, function () {
+          msnry.layout();
       });
   },
 
   render: function () {
     var self = this;
-    console.log(self.props.items[0]);
     return (
     <div id='gallery'>
       {self.props.items.map(function (item, index) {
         var img = query.getFirstImage(
                   query.removeAds(item.summary.content));
-        img.className = 'inner-image img-responsive';
         var wrapper = document.createElement('div');
+
+        img.className = 'inner-image img-responsive';
         wrapper.appendChild(img);
         return (
         <div className='image-post'>
