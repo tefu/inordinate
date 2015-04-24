8ec194dc89d801b6b156767985816c3962c2f94b
Trevor Hullinger <hakoniw@gmail.com>
2015-04-23 21:22:35 -0600

    Add more CSS frosting to the sidebar

--- src/jsx/sidebar.jsx
+++ src/jsx/sidebar.jsx
@@ -15,16 +15,18 @@ var Sidebar = React.createClass({
   render: function () {
     var self = this;
     return (
-    <ul className='list-group'>
+    <ul className='subscription-list'>
       {self.props.subscriptions.map(function (sub, i) {
         return (
-        <li className={'list-group-item truncate ' +
+        <li className={'subscription ' +
                        ((i === self.state.active) ?
-                       'active-subscription' :
-                       'subscription')}
+                       'active' :
+                       '')}
             key={sub.id}
             onClick={function () {self.handleClick(sub, i);}}>
-          <img className='sub-image' src={sub.iconUrl}>{' ' + sub.title}</img>
+          <img className='sub-image' src={sub.iconUrl}></img>
+          <span className='sub-title truncate'>{sub.title}</span>
+          <span className='unread-count badge'>12</span>
         </li>
         );
         })}
